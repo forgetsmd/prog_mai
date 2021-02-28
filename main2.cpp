e89a80c3b1d70620b4f7f52dc108c14d4a45963c
forgetsmd <forgetsmd@gmail.com>
2021-02-28 22:42:14 +0300

    sec

--- main2.cpp
+++ main2.cpp
@@ -5,22 +5,21 @@ using namespace std;
 
 int main (void) {
 	int sum = 0, digit = 0;
+	char flow;
 	string str;
 	cout << "Enter a string ";
 	cin >> str;
 	for (int i = 0; i < str.size(); i++) {
 		if (isdigit(str[i])) {
 			digit++;
-			sum += atoi(&str[i]);
+			flow = str[i];
+			sum += atoi(&flow);
 		}
-		else {
+		if (!isdigit(str[i])) {
 			cout << (char) tolower(str[i]);
 		}
 	}
-	cout << "The number of digits: " << digit << endl;
+	cout << endl << "The number of digits: " << digit << endl;
 	cout << "Sum: " << sum;
-	int out;
-	cout << endl << "Enter a symbol for quit: ";
-	cin >> out ;
 	return 0;
 }
